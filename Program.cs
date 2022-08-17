096acdb1703342f8d6518839d840f854e34a6afc
M4jxr <cergeirizhuk@gmail.com>
2022-08-17 11:32:24 +0200

    1commit

---
+++ Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+class Homework
+{
+    public static void Main()
+    {
+        int[] arr = { 64, 34, 25, 12, 22, 11, 90 };
+        bubbleSort(arr);
+        Console.WriteLine("Sorted array");
+        printArray(arr);
+
+        int[] arr2 = { 64, 25, 12, 22, 11 };
+        sort(arr);
+        Console.WriteLine("Sorted array");
+        printArray(arr);
+
+        int[] arr3 = { 12, 11, 13, 5, 6 };
+        InsertionSort ob = new InsertionSort();
+        ob.sort(arr);
+        printArray(arr);
+
+    }
+
+
+    static void sort(int[] arr)
+    {
+        int n = arr.Length;
+
+        // One by one move boundary of unsorted subarray
+        for (int i = 0; i < n - 1; i++)
+        {
+            // Find the minimum element in unsorted array
+            int min_idx = i;
+            for (int j = i + 1; j < n; j++)
+                if (arr[j] < arr[min_idx])
+                    min_idx = j;
+
+            // Swap the found minimum element with the first
+            // element
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    // Prints the array
+    static void printArray(int[] arr2)
+    {
+        int n = arr2.Length;
+        for (int i = 0; i < n; ++i)
+            Console.Write(arr2[i] + " ");
+        Console.WriteLine();
+    }
+
+    static void bubbleSort(int[] arr)
+    {
+        int n = arr.Length;
+        for (int i = 0; i < n - 1; i++)
+            for (int j = 0; j < n - i - 1; j++)
+                if (arr[j] > arr[j + 1])
+                {
+                    // swap temp and arr[i]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+    }
+
+    /* Prints the array */
+    static void printArray1(int[] arr)
+    {
+        int n = arr.Length;
+        for (int i = 0; i < n; ++i)
+            Console.Write(arr[i] + " ");
+        Console.WriteLine();
+    }
+
+
+    // Function to sort array
+    // using insertion sort
+    void sort1(int[] arr)
+    {
+        int n = arr.Length;
+        for (int i = 1; i < n; ++i)
+        {
+            int key = arr[i];
+            int j = i - 1;
+
+            // Move elements of arr[0..i-1],
+            // that are greater than key,
+            // to one position ahead of
+            // their current position
+            while (j >= 0 && arr[j] > key)
+            {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    // A utility function to print
+    // array of size n
+    static void printArray2(int[] arr)
+    {
+        int n = arr.Length;
+        for (int i = 0; i < n; ++i)
+            Console.Write(arr[i] + " ");
+
+        Console.Write("\n");
+    }
+}
+
+  
